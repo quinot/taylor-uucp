a5a63ab17be746605af7fcfc062385653cbcaf8b
Ian Lance Taylor <ian@airs.com>
1992-06-06 17:37:49 +0000

    Initial revision

---
+++ lib/xmall.c
@@ -0,0 +1,16 @@
+/* xmalloc.c
+   Allocate a block of memory without fail.  */
+
+#include "uucp.h"
+
+pointer
+xmalloc (c)
+     size_t c;
+{
+  pointer pret;
+
+  pret = malloc (c);
+  if (pret == NULL && c != 0)
+    ulog (LOG_FATAL, "Out of memory");
+  return pret;
+}
