c336e6fdb8525f88b4e980761c05aab90417d2b5
Ian Lance Taylor <ian@airs.com>
1992-06-11 18:11:09 +0000

    Simplified implementation

--- unix/rename.c
+++ unix/rename.c
@@ -8,8 +8,6 @@
 #endif
 
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /* This implementation will not work on directories, but fortunately
    we never want to rename directories.  */
@@ -19,27 +17,13 @@ rename (zfrom, zto)
      const char *zfrom;
      const char *zto;
 {
-  /* Try to make the link without removing the old file first.  */
   if (link (zfrom, zto) < 0)
     {
-      struct stat sfrom, sto;
-
       if (errno != EEXIST)
 	return -1;
-
-      if (strcmp (zfrom, zto) == 0)
-	return 0;
-      if (stat (zfrom, &sfrom) < 0
-	  || stat (zto, &sto) < 0)
-	return -1;
-      if (sfrom.st_ino == sto.st_ino
-	  && sfrom.st_dev == sto.st_dev)
-	return 0;
-
       if (unlink (zto) < 0
 	  || link (zfrom, zto) < 0)
 	return -1;
     }
-
   return unlink (zfrom);
 }
