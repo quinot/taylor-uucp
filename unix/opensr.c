c1de6289e81d58d067ffd59b9ecc9a3be42e96ad
Ian Lance Taylor <ian@airs.com>
1993-06-04 03:17:01 +0000

    Updated copyrights

--- unix/opensr.c
+++ unix/opensr.c
@@ -1,7 +1,7 @@
 /* opensr.c
    Open files for sending and receiving.
 
-   Copyright (C) 1991, 1992 Ian Lance Taylor
+   Copyright (C) 1991, 1992, 1993 Ian Lance Taylor
 
    This file is part of the Taylor UUCP package.
 
