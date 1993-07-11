68071fab61ce133ade07290f74beb6d59bd5e78f
Ian Lance Taylor <ian@airs.com>
1993-07-11 18:56:23 +0000

    Updated copyright to 1993

--- unix/xqtfil.c
+++ unix/xqtfil.c
@@ -1,7 +1,7 @@
 /* xqtfil.c
    Routines to read execute files.
 
-   Copyright (C) 1991, 1992 Ian Lance Taylor
+   Copyright (C) 1991, 1992, 1993 Ian Lance Taylor
 
    This file is part of the Taylor UUCP package.
 
