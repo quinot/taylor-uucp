bacdf565cde2ab5a197aceb184f0a507fc204cc3
Ian Lance Taylor <ian@airs.com>
1993-09-02 03:02:24 +0000

    Updated copyright

--- uuconf/tcalou.c
+++ uuconf/tcalou.c
@@ -1,7 +1,7 @@
 /* tcalou.c
    Find callout login name and password from Taylor UUCP configuration files.
 
-   Copyright (C) 1992 Ian Lance Taylor
+   Copyright (C) 1992, 1993 Ian Lance Taylor
 
    This file is part of the Taylor UUCP uuconf library.
 
