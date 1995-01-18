dfb54d1b5eb3ab39eb38ee612f18c2e483f2e6bb
Ian Lance Taylor <ian@airs.com>
1995-01-18 03:49:57 +0000

    Don't get confused by a double slash

--- unix/mkdirs.c
+++ unix/mkdirs.c
@@ -28,6 +28,11 @@ fsysdep_make_dirs (zfile, fpublic)
     {
       if (*z == '/' && z != zcopy)
 	{
+	  /* Some versions of uuto will send a double slash.  Some
+             systems will fail to create a directory ending in a
+             slash.  */
+	  if (z[-1] == '/')
+	    continue;
 	  *z = '\0';
 	  if (mkdir (zcopy, imode) != 0
 	      && errno != EEXIST
