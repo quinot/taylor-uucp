a41370550b57015700655a813a8eb382ceb58443
Ian Lance Taylor <ian@airs.com>
1995-05-26 20:32:32 +0000

    Update copyright

--- uupick.c
+++ uupick.c
@@ -1,7 +1,7 @@
 /* uupick.c
    Get files stored in the public directory by uucp -t.
 
-   Copyright (C) 1992, 1993, 1994 Ian Lance Taylor
+   Copyright (C) 1992, 1993, 1994, 1995 Ian Lance Taylor
 
    This file is part of the Taylor UUCP package.
 
