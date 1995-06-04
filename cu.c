94c11de75acb7322e5c19d199aca7d7bd4af0343
Ian Lance Taylor <ian@airs.com>
1995-06-04 19:41:45 +0000

    Update copyright

--- cu.c
+++ cu.c
@@ -1,7 +1,7 @@
 /* cu.c
    Call up a remote system.
 
-   Copyright (C) 1992, 1993, 1994 Ian Lance Taylor
+   Copyright (C) 1992, 1993, 1994, 1995 Ian Lance Taylor
 
    This file is part of the Taylor UUCP package.
 
