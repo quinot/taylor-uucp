563710a07cb8884fd88edeabfce53b73395e3929
Ian Lance Taylor <ian@airs.com>
1995-08-02 00:24:28 +0000

    Update copyright

--- prote.c
+++ prote.c
@@ -1,7 +1,7 @@
 /* prote.c
    The 'e' protocol.
 
-   Copyright (C) 1991, 1992 Ian Lance Taylor
+   Copyright (C) 1991, 1992, 1995 Ian Lance Taylor
 
    This file is part of the Taylor UUCP package.
 
